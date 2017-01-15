4f5cee5fe2d4c86a981b5d06485fcc6d1d3aa621
vviital <vviital@gmail.com>
2017-01-15 13:01:00 +0300

    Make code less error-prone

--- src/lib/scrap.js
+++ src/lib/scrap.js
@@ -2,13 +2,26 @@ const http = require('http');
 const concat = require('concat-stream');
 const through = require('through2');
 const parse = require('./parser');
+const logger = require('./logger');
 
-function scrap(url, fn) {
-  const stream = through({ objectMode: true }, fn);
-  http.get(url, (res) => {
+function sendRequest(url, stream, tryAgain) {
+  const request = http.get(url, (res) => {
     res.setEncoding('utf8');
     res.pipe(concat(body => stream.end(parse(body, { url }))));
-  });
+  })
+    .on('error', (error) => {
+      logger.error(`${url} - ${error.message}`);
+      if (tryAgain) {
+        sendRequest(url, stream, tryAgain - 1);
+      }
+    });
+  return request;
+}
+
+function scrap(url, fn, tryAgain = 5) {
+  const stream = through({ objectMode: true }, fn);
+
+  sendRequest(url, stream, tryAgain);
 
   return stream;
 }
